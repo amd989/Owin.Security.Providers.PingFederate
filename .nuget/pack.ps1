36be3441e9f55e745f8e535be9c63d8dbba0cc2b
amd989 <alejandro@shift-me.com>
2015-05-05 21:34:03 -0600

    Revert "Changing nuget packaging method"
    This reverts commit 524a4d7ff46ab5d251f7b91140cda614c2ae88b0.

---
+++ .nuget/pack.ps1
@@ -0,0 +1,12 @@
+$root = $env:APPVEYOR_BUILD_FOLDER
+$version = [System.Reflection.Assembly]::LoadFile("$root\Owin.Security.Providers.PingFederate\bin\Release\Owin.Security.Providers.PingFederate.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\.nuget\Owin.Security.Providers.PingFederate.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\.nuget\Owin.Security.Providers.PingFederate.compiled.nuspec
+
+& $root\.nuget\NuGet.exe pack $root\.nuget\Owin.Security.Providers.PingFederate.compiled.nuspec
